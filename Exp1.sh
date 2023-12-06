2e846fd2c7f3970ec303ae85bf39de79ea95cde5
HarshvardhanPandey2003 <harshvardhanpandey2003@gmail.com>
2023-12-06 18:50:41 +0530

    Update Exp1.sh

--- Exp1.sh
+++ Exp1.sh
@@ -1,18 +1,15 @@
-{
- "cells": [
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "metadata": {},
-   "outputs": [],
-   "source": []
-  }
- ],
- "metadata": {
-  "language_info": {
-   "name": "python"
-  }
- },
- "nbformat": 4,
- "nbformat_minor": 2
-}
+#!/bin/bash
+
+echo "Enter a number:"
+read num
+
+reverse=0
+
+while [ $num -gt 0 ]; 
+do
+  digit=$((num % 10))
+  reverse=$((reverse * 10 + digit))
+  num=$((num / 10))
+done
+
+echo "Reversed number: $reverse"
